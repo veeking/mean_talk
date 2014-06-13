f693d50c3225ad9d3922593a64630f53c9d4eb4c
veeking <445915818@qq.com>
2014-06-13 23:51:15 +0800

    0.0.3

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
+ var express = require('express');
+ var app = express();
+ var port = process.env.PORT || 3000;
 
-/**
- * Module dependencies.
- */
+  app.use(express.static(__dirname + '/static'));
+  app.use(function(req,res){
+  		 res.sendfile('./static/index.html');
+  });
+  var io = require('socket.io').listen(app.listen(port));
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  var messages = [];  //  将消息暂存到缓存里
+  io.sockets.on('connection',function(socket){
+  	   socket.on('getAllMessages',function(){
+  	     socket.emit('allMessages',messages);				
+  	   });
+	   socket.on('createMessage',function(message){
+	   	 messages.push(message);
+  	     io.sockets.emit('messageAdded',message);				
+  	   });
+ 
+  });
+  console.log('app is runing' + port);
